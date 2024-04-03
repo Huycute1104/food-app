4a16d38756705221343933302c297915c480e99e
Huy Pham <huypt110402@gmail.com>
2024-04-04 00:58:20 +0700

    refresh token

--- src/main/java/com/example/food/cotroller/AuthenticationController.java
+++ src/main/java/com/example/food/cotroller/AuthenticationController.java
@@ -4,6 +4,8 @@ import com.example.food.auth.AuthenticationRequest;
 import com.example.food.auth.AuthenticationResponse;
 import com.example.food.auth.AuthenticationService;
 import com.example.food.config.LogoutService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -24,6 +28,14 @@ public class AuthenticationController {
     @PostMapping("/login")
     public ResponseEntity<AuthenticationResponse> login(@RequestBody AuthenticationRequest authenticationRequest) {
         return ResponseEntity.ok(authenticationService.login(authenticationRequest));
+    }
+    @PostMapping("/refresh-token")
+    public void refreshToken(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) throws IOException {
+        authenticationService.refreshToken(request,response);
+
     }
 
 }
