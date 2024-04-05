a9a956393be36f4da51b27667b384159ac2034c8
Huy Pham <huypt110402@gmail.com>
2024-04-05 15:53:26 +0700

    update api create category

--- src/main/java/com/example/food/serviceImplement/CategoryServiceImplement.java
+++ src/main/java/com/example/food/serviceImplement/CategoryServiceImplement.java
@@ -12,23 +12,32 @@ import org.springframework.stereotype.Service;
 public class CategoryServiceImplement implements CategoryService {
     @Autowired
     private CategoryRepo categoryRepo;
+
     @Override
     public CategoryResponse createCategory(CreateCategoryRequest request) {
         //get category name
         String name = request.getCategoryName();
         //check exist category
         var category = categoryRepo.findCategoriesByCategoryName(name).orElse(null);
-        if(category == null){
-            Category cate =  Category.builder()
-                    .categoryName(name)
-                    .build();
-            categoryRepo.save(cate);
-            return  CategoryResponse.builder()
-                    .status("Create new category successfully")
-                    .category(cate)
-                    .build();
-        }else {
-            return  CategoryResponse.builder()
+        if (category == null) {
+            if (isValidName(name)) {
+                Category cate = Category.builder()
+                        .categoryName(name)
+                        .build();
+                categoryRepo.save(cate);
+                return CategoryResponse.builder()
+                        .status("Create new category successfully")
+                        .category(cate)
+                        .build();
+            }else{
+                return CategoryResponse.builder()
+                        .status("The Category Name must be between 3 and 20 char and should not contain any special characters.")
+                        .category(null)
+                        .build();
+            }
+
+        } else {
+            return CategoryResponse.builder()
                     .status("Category is exist")
                     .category(null)
                     .build();
@@ -37,30 +46,31 @@ public class CategoryServiceImplement implements CategoryService {
 
     private boolean isValidName(String name) {
         //Check validate category name
-        return name != null && name.length() <=20 && name.length()>=3 && !name.matches(".*[^a-zA-Z0-9].*");
+        return name != null && name.length() <= 20 && name.length() >= 3 && !name.matches(".*[^a-zA-Z0-9].*");
     }
+
     @Override
-    public CategoryResponse updateCategory(CreateCategoryRequest request,int ID) {
+    public CategoryResponse updateCategory(CreateCategoryRequest request, int ID) {
         //get category name
         String name = request.getCategoryName();
         //check exist category
         var category = categoryRepo.findCategoriesByCategoryId(ID).orElse(null);
-        if(category != null){
-            if(isValidName(name)){
+        if (category != null) {
+            if (isValidName(name)) {
                 category.setCategoryName(name);
                 categoryRepo.save(category);
-                return  CategoryResponse.builder()
+                return CategoryResponse.builder()
                         .status("Update category successfully")
                         .category(category)
                         .build();
             } else {
-                return  CategoryResponse.builder()
+                return CategoryResponse.builder()
                         .status("The Category Name must be between 3 and 20 char and should not contain any special characters.")
                         .category(null)
                         .build();
             }
-        }else{
-            return  CategoryResponse.builder()
+        } else {
+            return CategoryResponse.builder()
                     .status("Category not found")
                     .category(null)
                     .build();
