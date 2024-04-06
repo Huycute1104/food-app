9d3de8b10658e53aa39c18896f2503fef61967cc
Huy Pham <huypt110402@gmail.com>
2024-04-06 21:17:50 +0700

    fix config swagger

--- src/main/java/com/example/food/config/SecurityConfig.java
+++ src/main/java/com/example/food/config/SecurityConfig.java
@@ -34,7 +34,17 @@ public class SecurityConfig {
                 .cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.configure(http))
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authentication -> authentication
-                        .requestMatchers("/api/v1/auth/**","/v3/api-docs","/swagger-ui.html")
+                        .requestMatchers("/api/v1/auth/**",
+                                "/v2/api-docs",
+                                "/v3/api-docs",
+//                                "/v3/api-docs/**",
+                                "/swagger-resources",
+                                "/swagger-resources/**",
+                                "/configuration/ui",
+                                "/configuration/security",
+                                "/swagger-ui/**",
+                                "/webjars/**",
+                                "/swagger-ui.html")
                         .permitAll()
                         .requestMatchers("api/v1/**")
                         .permitAll()
