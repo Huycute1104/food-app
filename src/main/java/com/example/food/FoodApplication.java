9ba2cfda62e2c4a8416f9bdc4d423190c1cca0da
Huy Pham <huypt110402@gmail.com>
2024-04-30 14:22:10 +0700

    update security config

--- src/main/java/com/example/food/FoodApplication.java
+++ src/main/java/com/example/food/FoodApplication.java
@@ -36,72 +36,72 @@ public class FoodApplication {
 				"api_secret", "KJfkzpiXRnmkPCeRwH6TUAmFGks"));
 		return cloudinary;
 	}
-	@Bean
-	public CommandLineRunner commandLineRunner(
-			AuthenticationService service,
-			CategoryService categoryService
-	) {
-		return args -> {
-			//Admin
-			var admin = RegisterRequest.builder()
-					.name("Admin")
-					.email("Admin@gmail.com")
-					.password("123")
-					.phone("0392272536")
-					.status(true)
-					.role(ADMIN)
-					.build();
-			System.out.println("Admin token :" + service.register(admin).getAccessToken());
-
-			//staff
-			var staff = RegisterRequest.builder()
-					.name("Staff")
-					.email("staff@gmail.com")
-					.status(true)
-					.password("123")
-					.phone("0854512367")
-					.role(STAFF)
-					.build();
-			System.out.println("Staff token :" + service.register(staff).getAccessToken());
-
-			//Customer
-			var customer = RegisterRequest.builder()
-					.name("Trần Huy")
-					.email("huypt110402@gmail.com")
-					.status(true)
-					.password("123")
-					.phone("0854512367")
-					.role(CUSTOMER)
-					.build();
-			System.out.println("Customer token :" + service.register(customer).getAccessToken());
-
-			var customer2 = RegisterRequest.builder()
-					.name("Huyền Trân")
-					.email("tran123@gmail.com")
-					.status(true)
-					.password("123")
-					.phone("0854512367")
-					.role(CUSTOMER)
-					.build();
-			service.register(customer2);
-
-			var category = CreateCategoryRequest.builder()
-					.categoryName("Drink")
-					.build();
-			categoryService.createCategory(category);
-
-			var category1 = CreateCategoryRequest.builder()
-					.categoryName("Seafood")
-					.build();
-			categoryService.createCategory(category1);
-
-			var category2 = CreateCategoryRequest.builder()
-					.categoryName("Snacks")
-					.build();
-			categoryService.createCategory(category2);
-
-		};
-	}
+//	@Bean
+//	public CommandLineRunner commandLineRunner(
+//			AuthenticationService service,
+//			CategoryService categoryService
+//	) {
+//		return args -> {
+//			//Admin
+//			var admin = RegisterRequest.builder()
+//					.name("Admin")
+//					.email("Admin@gmail.com")
+//					.password("123")
+//					.phone("0392272536")
+//					.status(true)
+//					.role(ADMIN)
+//					.build();
+//			System.out.println("Admin token :" + service.register(admin).getAccessToken());
+//
+//			//staff
+//			var staff = RegisterRequest.builder()
+//					.name("Staff")
+//					.email("staff@gmail.com")
+//					.status(true)
+//					.password("123")
+//					.phone("0854512367")
+//					.role(STAFF)
+//					.build();
+//			System.out.println("Staff token :" + service.register(staff).getAccessToken());
+//
+//			//Customer
+//			var customer = RegisterRequest.builder()
+//					.name("Trần Huy")
+//					.email("huypt110402@gmail.com")
+//					.status(true)
+//					.password("123")
+//					.phone("0854512367")
+//					.role(CUSTOMER)
+//					.build();
+//			System.out.println("Customer token :" + service.register(customer).getAccessToken());
+//
+//			var customer2 = RegisterRequest.builder()
+//					.name("Huyền Trân")
+//					.email("tran123@gmail.com")
+//					.status(true)
+//					.password("123")
+//					.phone("0854512367")
+//					.role(CUSTOMER)
+//					.build();
+//			service.register(customer2);
+//
+//			var category = CreateCategoryRequest.builder()
+//					.categoryName("Drink")
+//					.build();
+//			categoryService.createCategory(category);
+//
+//			var category1 = CreateCategoryRequest.builder()
+//					.categoryName("Seafood")
+//					.build();
+//			categoryService.createCategory(category1);
+//
+//			var category2 = CreateCategoryRequest.builder()
+//					.categoryName("Snacks")
+//					.build();
+//			categoryService.createCategory(category2);
+//
+//		};
+//	}
 
 	@GetMapping("")
 	public String greeting() {
--- src/main/java/com/example/food/config/SecurityConfig.java
+++ src/main/java/com/example/food/config/SecurityConfig.java
@@ -34,19 +34,7 @@ public class SecurityConfig {
                 .cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.configure(http))
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authentication -> authentication
-                        .requestMatchers("/api/v1/auth/**",
-                                "/v2/api-docs",
-                                "/v3/api-docs",
-//                                "/v3/api-docs/**",
-                                "/swagger-resources",
-                                "/swagger-resources/**",
-                                "/configuration/ui",
-                                "/configuration/security",
-                                "/swagger-ui/**",
-                                "/webjars/**",
-                                "/swagger-ui.html")
-                        .permitAll()
-                        .requestMatchers("api/v1/**")
+                        .requestMatchers("/**")
                         .permitAll()
                         .anyRequest()
                         .authenticated())
