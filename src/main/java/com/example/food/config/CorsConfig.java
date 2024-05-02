6071c603a48f76eb520dd8e3788e7279b4d864a1
Huy Pham <huypt110402@gmail.com>
2024-05-02 23:35:16 +0700

    config frontend

---
+++ src/main/java/com/example/food/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.example.food.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173") // Thay đổi origin này cho phù hợp với origin của ứng dụng frontend của bạn
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+
+}
